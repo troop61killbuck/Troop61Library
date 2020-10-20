2b3b7314cdef8fdd57927bf7e46e97d1737c2156
Troop 61 Killbuck <troop61woosterlibrary@gmail.com>
2020-10-20 19:08:02 -0400

    Update verify_email.php

--- sendgrid/verify_email.php
+++ sendgrid/verify_email.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('includes/sendgrid_credentials.php');
-
+$root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
@@ -12,7 +12,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\"personalizations\":[{\"to\":[{\"email\":\"$email\",\"name\":\"$name[0] $name[1]\"}],\"dynamic_template_data\":{\"url\":\"troop61library.com/admin/verifyemail/index.php?verificationCode=$code&email=$email&id=$id\"},\"subject\":\"Please Verify Your Email - Troop 61 Library Administration\"}],\"from\":{\"email\":\"$send_email\",\"name\":\"Troop 61 Library\"},\"reply_to\":{\"email\":\"$send_email\",\"name\":\"Troop 61 Library\"},\"template_id\":\"d-4034cba9971e471b9a374a86c090215b\"}",
+  CURLOPT_POSTFIELDS => "{\"personalizations\":[{\"to\":[{\"email\":\"$email\",\"name\":\"$name[0] $name[1]\"}],\"dynamic_template_data\":{\"url\":\"$rootadmin/verifyemail/index.php?verificationCode=$code&email=$email&id=$id\"},\"subject\":\"Please Verify Your Email - Troop 61 Library Administration\"}],\"from\":{\"email\":\"$send_email\",\"name\":\"Troop 61 Library\"},\"reply_to\":{\"email\":\"$send_email\",\"name\":\"Troop 61 Library\"},\"template_id\":\"d-4034cba9971e471b9a374a86c090215b\"}",
   CURLOPT_HTTPHEADER => array(
     "authorization: Bearer $apiKey",
     "content-type: application/json"
