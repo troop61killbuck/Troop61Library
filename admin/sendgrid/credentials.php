ec02f947b6b8216abb31b5a99ff0da612e8f4b6d
Troop 61 Killbuck <troop61woosterlibrary@gmail.com>
2020-12-14 14:43:59 -0500

    Delete credentials.php

--- admin/sendgrid/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php 
-//$apiKey = "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";
-
-//$send_email = "admin@troop61library.com";
-include('../sendgrid_credentials.php');
-
-?>
